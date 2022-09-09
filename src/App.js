b8ee4d88a8cdffc4dab5b88febb2ac317b308aea
Mounika Halember <84342199+MounikaHalember@users.noreply.github.com>
2022-09-09 15:28:44 +0530

    app.js-ref

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React from 'react';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent'
+import { Provider } from 'react-redux';
+import store from './store/store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
       ReactJS CRUD
       </header>
       <CreateEmployeeComponent />
     </div>
+    </Provider>
   );
 }
 
