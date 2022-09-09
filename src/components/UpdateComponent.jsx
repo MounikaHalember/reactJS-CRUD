8799d4770ebfd874d08404235574429cd91e5848
Mounika Halember <84342199+MounikaHalember@users.noreply.github.com>
2022-09-09 15:26:41 +0530

    refactor-update

--- src/components/UpdateComponent.jsx
+++ src/components/UpdateComponent.jsx
@@ -1,13 +1,17 @@
 import React,{useState,useEffect} from 'react'
-import {updateEmployee} from '../service/apiService'
+import { useDispatch,useSelector } from 'react-redux';
+import {updateEmployee,getEmployees} from '../service/apiService'
 import Button from 'react-bootstrap/Button';
-
+// import employeeDataArray from '../store/reducer';
+import setEmployeeDataArray from '../store/action';
 
 function UpdateComponent(props) {
-  const {employeeDataArray, setDataArray} = props;
+//   const {employeeDataArray, setDataArray} = props;
     const [updateData,setUpdateData] = useState({name:'',id:'',age:'',salary:''});
-    const [updateDataArray,setUpdateDataArray] = useState([]);
+    const employeeDataArray= useSelector(state => state.employeeDataArray);
 
+    let updateDataArray = [];
+  const dispatch = useDispatch();
     const updateNameHandler = (e)=>{
         setUpdateData({...updateData,name:e.target.value})
     }
@@ -18,14 +22,18 @@ function UpdateComponent(props) {
         setUpdateData({...updateData,id:e.target.value})
     }
     const updateEmployeeHandler =()=>{
-        updateEmployee(updateData,updateData.id).then(response=>{
-            console.log(response)
+       
             employeeDataArray.forEach(data=>{
-                if(data.id === updateData.id){
-                    setDataArray({...data,name:updateData.name})
-                }
+                console.log(data,updateData)
+                if(Number(data.id) === Number(updateData.id)){
+                    
+                    updateDataArray.push(updateData)
+                    console.log(updateData)
+                }else{updateDataArray.push(data)}
             })
-        })
+            console.log(updateDataArray)
+            dispatch(setEmployeeDataArray(updateDataArray))
+       
       }
   return (
     <div>
